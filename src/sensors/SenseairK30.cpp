b56b50009f1c28049232981b898ce4ebf0b903b2
Matt Covington <speleophysics@gmail.com>
2024-09-06 13:28:21 -0500

    first attempt at SenseairK30

---
+++ src/sensors/SenseairK30.cpp
@@ -0,0 +1,203 @@
+/**
+ * @file SenseairK30.cpp
+ * @copyright Stroud Water Research Center
+ * Part of the EnviroDIY ModularSensors library for Arduino.
+ * This library is published under the BSD-3 license.
+ * @author Matthew D. Covington <mcoving@uark.edu>
+ *
+ * @brief Implements the SenseairK30 class.
+ */
+
+#include "SenseairK30.h"
+
+byte  read_CO2[]    = {0xFE, 0X44, 0X00, 0X08, 0X02, 0X9F, 0X25};
+float valMultiplier = 10;
+
+SenseairK30::SenseairK30(Stream* stream, int8_t powerPin, int8_t triggerPin,
+                         uint8_t measurementsToAverage)
+    : Sensor("SenseairK30", K30_NUM_VARIABLES, K30_WARM_UP_TIME_MS,
+             K30_STABILIZATION_TIME_MS, K30_MEASUREMENT_TIME_MS, powerPin, -1,
+             measurementsToAverage),
+      _triggerPin(triggerPin),
+      _stream(stream) {}
+SenseairK30::SenseairK30(Stream& stream, int8_t powerPin, int8_t triggerPin,
+                         uint8_t measurementsToAverage)
+    : Sensor("MaxBotixMaxSonar", K30_NUM_VARIABLES, K30_WARM_UP_TIME_MS,
+             K30_STABILIZATION_TIME_MS, K30_MEASUREMENT_TIME_MS, powerPin, -1,
+             measurementsToAverage, K30_INC_CALC_VARIABLES),
+      _triggerPin(triggerPin),
+      _stream(&stream) {}
+// Destructor
+SenseairK30::~SenseairK30() {}
+
+
+// unfortunately, we really cannot know where the stream is attached.
+String SenseairK30::getSensorLocation(void) {
+    // attach the trigger pin to the stream number
+    String loc = "co2Stream_trigger" + String(_triggerPin);
+    return loc;
+}
+
+
+bool SenseairK30::setup(void) {
+    // Set up the trigger, if applicable
+    if (_triggerPin >= 0) {
+        pinMode(_triggerPin, OUTPUT);
+        digitalWrite(_triggerPin, LOW);
+    }
+
+    // Set the stream timeout
+    // Even the slowest sensors should respond at a rate of 6Hz (166ms).
+    _stream->setTimeout(2500);
+
+    return Sensor::setup();  // this will set pin modes and the setup status bit
+}
+
+
+// Parsing and tossing the header lines in the wake-up
+bool SenseairK30::wake(void) {
+    // Sensor::wake() checks if the power pin is on and sets the wake timestamp
+    // and status bits.  If it returns false, there's no reason to go on.
+    if (!Sensor::wake()) return false;
+
+    // NOTE: After the power is turned on to the MaxBotix, it sends several
+    // lines of header to the serial port, beginning at ~65ms and finising at
+    // ~160ms. Although we are waiting for them to complete in the
+    // "waitForWarmUp" function, the values will still be in the serial buffer
+    // and need to be read to be cleared out For an HRXL without temperature
+    // compensation, the headers are: HRXL-MaxSonar-WRL PN:MB7386 Copyright
+    // 2011-2013 MaxBotix Inc. RoHS 1.8b090  0713 TempI
+
+    // NOTE ALSO:  Depending on what type of serial stream you are using, there
+    // may also be a bunch of junk in the buffer that this will clear out.
+    /** MS_DBG(F("Dumping Header Lines from K30 on"), getSensorLocation());
+        for (int i = 0; i < 6; i++) {
+            String headerLine = _stream->readStringUntil('\r');
+            MS_DBG(i, '-', headerLine);
+        }
+        // Clear anything else out of the stream buffer
+        auto junkChars = static_cast<uint8_t>(_stream->available());
+        if (junkChars) {
+            MS_DBG(F("Dumping"), junkChars,
+                   F("characters from K30 stream buffer"));
+            for (uint8_t i = 0; i < junkChars; i++) {
+    #ifdef MS_SenseairK30_DEBUG
+                DEBUGGING_SERIAL_OUTPUT.print(_stream->read());
+    #else
+                _stream->read();
+    #endif
+            }
+    #ifdef MS_SENSEAIRK30_DEBUG
+            DEBUGGING_SERIAL_OUTPUT.println();
+    #endif
+        }
+    **/
+    return true;
+}
+
+
+bool SenseairK30::addSingleMeasurementResult(void) {
+    // Initialize values
+    bool    success = false;
+    int16_t result  = -9999;
+    /**
+        // Clear anything out of the stream buffer
+        auto junkChars = static_cast<uint8_t>(_stream->available());
+        if (junkChars) {
+            MS_DBG(F("Dumping"), junkChars,
+                   F("characters from K30 stream buffer:"));
+            for (uint8_t i = 0; i < junkChars; i++) {
+    #ifdef MS_SENSEAIRK30_DEBUG
+                DEBUGGING_SERIAL_OUTPUT.print(_stream->read());
+    #else
+                _stream->read();
+    #endif
+            }
+    #ifdef MS_SENSEAIRK30_DEBUG
+            DEBUGGING_SERIAL_OUTPUT.println();
+    #endif
+
+        }
+    */
+    // Check a measurement was *successfully* started (status bit 6 set)
+    // Only go on to get a result if it was
+    //  if (bitRead(_sensorStatus, 6)) {*/
+    MS_DBG(getSensorNameAndLocation(), F("is reporting:"));
+
+    uint8_t rangeAttempts = 0;
+    while (success == false && rangeAttempts < 25) {
+        // If the sonar is running on a trigger, activating the trigger
+        // should in theory happen within the startSingleMeasurement
+        // function.  Because we're really taking up to 25 measurements
+        // for each "single measurement" until a valid value is returned
+        // and the measurement time is <166ms, we'll actually activate
+        // the trigger here.
+        /*if (_triggerPin >= 0) {
+            MS_DBG(F("  Triggering Sonar with"), _triggerPin);
+            digitalWrite(_triggerPin, HIGH);
+            delayMicroseconds(30);  // Trigger must be held high for >20 µs
+            digitalWrite(_triggerPin, LOW);
+        }*/
+        int timeout = 0;
+        while (!_stream->available()) {
+            _stream->write(readCO2, 7);
+            delay(50);
+        }
+
+        timeout = 0;
+        while (_stream->available() < 7) {
+            timeout++;
+            if (timeout > 10) {
+                while (_stream->available()) { _stream->read(); }
+
+                break;
+            }
+            delay(50);
+        }
+        if (_stream->available() >= 7) {
+            byte packet[7];
+            _stream->readBytes(packet, 7);
+
+            // Calculate value
+            int high = packet[3];
+            int low  = packet[4];
+            result   = (high * 256 + low) * valMultiplier;
+
+            //_stream->read();  // To throw away the carriage return
+            MS_DBG(F("  CO2:"), result);
+        } else {
+            result = -9999;
+        }
+        rangeAttempts++;
+
+        // If it cannot obtain a result , the sonar is supposed to send a
+        // value just above it's max range.  For 10m models, this is 9999,
+        // for 5m models it's 4999.  The sonar might also send readings of
+        // 300 or 500 (the blanking distance) if there are too many acoustic
+        // echos. If the result becomes garbled or the sonar is
+        // disconnected, the parseInt function returns 0.  Luckily, these
+        // sensors are not capable of reading 0, so we also know the 0 value
+        // is bad.
+        if (result <= 0) {
+            MS_DBG(F("  Bad or Suspicious Result, Retry Attempt #"),
+                   rangeAttempts);
+            result = -9999;
+        } else {
+            MS_DBG(F("  Good result found"));
+            success = true;
+        }
+        /**}
+    } else {
+        MS_DBG(getSensorNameAndLocation(), F("is not currently measuring!"));
+    }*/
+
+        verifyAndAddMeasurementResult(K30_VAR_NUM, result);
+
+        // Unset the time stamp for the beginning of this measurement
+        _millisMeasurementRequested = 0;
+        // Unset the status bits for a measurement request (bits 5 & 6)
+        //_sensorStatus &= 0b10011111;
+
+        // Return values shows if we got a not-obviously-bad reading
+        return success;
+    }
---
+++ src/sensors/SenseairK30.h
@@ -0,0 +1,290 @@
+/**
+ * @file SenseairK30.h
+ * @copyright Stroud Water Research Center
+ * Part of the EnviroDIY ModularSensors library for Arduino.
+ * This library is published under the BSD-3 license.
+ * @author Matthew D. Covington <mcoving@uark.edu>
+ *
+ * @brief Contains the SenseairK30 sensor subclass and the SenseairK30_CO2
+ * variable subclass.
+ *
+ * These are for the Senseair K30 carbon dioxide sensor.
+ */
+/* clang-format off */
+/**
+ * @defgroup sensor_senseair MaxBotix MaxSonar
+ * Classes for the Senseair K30 carbon dioxide sensor.
+ *
+ * @ingroup the_sensors
+ *
+ * @tableofcontents
+ * @m_footernavigation
+ *
+ * @section sensor_maxbotix_intro Introduction
+ *
+ * The IP67 rated HRXL-MaxSonar-WR ultrasonic rangefinders offer 1mm
+ * resolution, 2.7-5.5VDC operation, a narrow beam pattern, high power output,
+ * noise rejection, automatic calibration, and temperature compensation.
+ * Depending on the precise model, the range finders have ranges between 300 and
+ * 9999mm and read rates of 6-7.5Hz.  This library supports TTL or RS323 sensor
+ * output, though an RS232-to-TTL adapter is needed for the RS232 models. Analog
+ * and pulse-width outputs are not supported.  The MaxBotix sensors require
+ * a 2.7V - 5.5V power supply to pin 6 on the sensor (which can be turned off
+ * between measurements) and the level of the TTL returned by the MaxSonar will
+ * match the power level it is supplied with.  The digital TTL or RS232 output
+ * is sent out on pin 5 on the sensor.  Pin 7 of the MaxSonar must be connected
+ * to power ground and pin 4 can optionally be used to trigger the MaxSonar.
+ *
+ * If you are using the
+ * [MaxBotix HR-MaxTemp](https://www.maxbotix.com/Ultrasonic_Sensors/MB7955.htm)
+ * MB7955 temperature compensator on your MaxBotix (which greatly improves data
+ * quality), the red wire from the MaxTemp should be attached to pin 1
+ * (square) on the MaxSonar.  The white and shield (bare silver) wires from the
+ * MaxTemp should both be attached to Pin 7 (GND).  The MaxTemp communicates
+ * directly with the MaxSonar and there is no need to make any changes on the
+ * Aruduino itself to accomodate it.  It is not possible to read the temperature
+ * data from the MaxTemp.
+ *
+ * The MaxBotix sensor have two different modes: free-ranging and triggered.
+ * Unless the trigger pin is externally held low, the sensor will continuously
+ * take readings at a rate of 6Hz or greater and immediate report each result
+ * over the digital output pin.  (That is, it will be in free-ranging mode.)
+ * When continuously powered and operating in free-range mode, the data output
+ * is automatically filtered to help improve accuracy.  If you are turning the
+ * power to the sensor off between readings, there is no advantage to using the
+ * free-ranging because many readings must be taken before the filter becomes
+ * effective.  In this case, you may save a very small amount of power by
+ * setting up a trigger pin and manually trigger individual readings.
+ *
+ * Please see the section
+ * "[Notes on Arduino Streams and Software Serial](@ref page_arduino_streams)"
+ * for more information about what streams can be used along with this library.
+ *
+ * This library supports using multiple MaxBotix sensors on the same logger,
+ * with a few caveats:
+ *  - Any sensor operating in free-ranging mode (powered at the same time as any
+ * other sensors with the trigger pins unconnected) must have a dedicated stream
+ * instance/serial port.
+ *  - To have two sensors operating in free-ranging mode, they must each have a
+ * dedicated stream instance/serial port *AND* you must specify a unique
+ * _negative_ pin number for the trigger pin. Giving a negative pin number
+ * ensures that the Arduino will not attempt to trigger trigger individual
+ * readings but will still be able to tell the sensors apart. (Software-wise,
+ * simply specifying the different streams is not enough!)  Keep in mind that
+ * two or more free ranging sensors must be spaced far enough apart in the field
+ * to prevent interference between the sonar beams.
+ *  - Two or more sensors may send data to the same stream instance/serial port
+ * if both sensors are being triggered and each is triggered by a different
+ * trigger pin.
+ *  - "Daisy chaining" sensors so the pulse-width output of one sensor acts as
+ * the trigger for a second sensor *is not supported*.
+ *
+ * @section sensor_maxbotix_datasheet Sensor Datasheet
+ * - [HRXL WR Datasheet](https://github.com/EnviroDIY/ModularSensors/wiki/Sensor-Datasheets/Maxbotix-HRXL-MaxSonar-WR-Datasheet.pdf)
+ * - [HRXL WRS Datasheet](https://github.com/EnviroDIY/ModularSensors/wiki/Sensor-Datasheets/Maxbotix-HRXL-MaxSonar-WRS-Datasheet.pdf)
+ * - [MaxTemp Datasheet](https://github.com/EnviroDIY/ModularSensors/wiki/Sensor-Datasheets/Maxbotix-HR-MaxTemp-Datasheet.pdf)
+ * - [Wiring Guide](https://github.com/EnviroDIY/ModularSensors/wiki/Sensor-Datasheets/Maxbotix-MaxSonar-MB7954-Datasheet-ConnectWire.pdf)
+ *
+ * @section sensor_maxbotix_ctor Sensor Constructor
+ * {{ @ref MaxBotixSonar::MaxBotixSonar }}
+ *
+ * ___
+ * @section sensor_maxbotix_examples Example Code
+ * The MaxBotix MaxSonar is used in the @menulink{max_botix_sonar} example.
+ *
+ * @menusnip{max_botix_sonar}
+ */
+/* clang-format on */
+
+// Header Guards
+#ifndef SRC_SENSORS_SENSEAIRK30_H_
+#define SRC_SENSORS_SENSEAIRK30_H_
+
+// Debugging Statement
+// #define MS_MAXBOTIXSONAR_DEBUG
+
+#ifdef MS_SENSEAIRK30_DEBUG
+#define MS_DEBUGGING_STD "SenseairK30"
+#endif
+
+// Included Dependencies
+#include "ModSensorDebugger.h"
+#undef MS_DEBUGGING_STD
+#include "VariableBase.h"
+#include "SensorBase.h"
+
+/** @ingroup sensor_senseair */
+/**@{*/
+
+// Sensor Specific Defines
+/// @brief Sensor::_numReturnedValues; the K30 can report 1 value.
+#define K30_NUM_VARIABLES 1
+/// @brief Sensor::_incCalcValues; we don't calculate any additional values.
+#define K30_INC_CALC_VARIABLES 0
+
+/**
+ * @anchor sensor_senseair_timing
+ * @name Sensor Timing
+ * The sensor timing for a Senseair K30 CO2 sensor
+ */
+/**@{*/
+/// @brief Sensor::_warmUpTime_ms; warm up time to completion of header:  160ms.
+#define K30_WARM_UP_TIME_MS 10000
+/// @brief Sensor::_stabilizationTime_ms; the HRXL is stable as soon as it warms
+/// up (0ms stabilization).
+#define K30_STABILIZATION_TIME_MS 0
+/// @brief Sensor::_measurementTime_ms; the HRXL takes 166ms to complete a
+/// measurement.
+#define K30_MEASUREMENT_TIME_MS 2500
+/**@}*/
+
+/**
+ * @anchor sensor_maxbotix_range
+ * @name Range
+ * The range variable from a Maxbotix HRXL ultrasonic range finder
+ * - Range is 300 to 5000mm or 500 to 9999mm, depending on model
+ * - Accuracy is ±1%
+ *
+ * {{ @ref MaxBotixSonar_Range::MaxBotixSonar_Range }}
+ */
+/**@{*/
+/// @brief Decimals places in string representation; range should have 0 -
+/// resolution is 1mm.
+#define K30_RESOLUTION 0
+/// @brief Sensor variable number; range is stored in sensorValues[0].
+#define K30_VAR_NUM 0
+/// @brief Variable name in
+/// [ODM2 controlled vocabulary](http://vocabulary.odm2.org/variablename/);
+/// "carbonDioxide"
+#define K30_VAR_NAME "carbonDioxide"
+/// @brief Variable unit name in
+/// [ODM2 controlled vocabulary](http://vocabulary.odm2.org/units/);
+/// "Part per Million"
+#define K30_UNIT_NAME "Part per Million"
+/// @brief Default variable short code; "SonarRange"
+#define K30_DEFAULT_CODE "CO2"
+/**@}*/
+
+
+/* clang-format off */
+/**
+ * @brief The Sensor sub-class for the
+ * [MaxBotix ultrasonic range finders](@ref sensor_maxbotix).
+ *
+ * @ingroup sensor_maxbotix
+ */
+/* clang-format on */
+class SenseairK30 : public Sensor {
+ public:
+    /**
+     * @brief Construct a new Senseair K30 object
+     *
+     * @param stream An Arduino data stream for TTL or RS232 communication.  See
+     * [notes](@ref page_arduino_streams) for more information on what streams
+     * can be used.
+     * @param powerPin The pin on the mcu controlling power to the K30.
+     * Use -1 if it is continuously powered.
+     * - The K30 requires a 5V - 14V power supply.
+     * @param triggerPin The pin on the mcu controlling the "trigger" for the
+     * K30.  Use -1 or omit for continuous ranging.
+     * @param measurementsToAverage The number of measurements to take and
+     * average before giving a "final" result from the sensor; optional with a
+     * default value of 1.
+     */
+    SenseairK30(Stream* stream, int8_t powerPin, int8_t triggerPin = -1,
+                uint8_t measurementsToAverage = 1);
+    /**
+     * @copydoc SenseairK30::SenseairK30
+     */
+    SenseairK30(Stream& stream, int8_t powerPin, int8_t triggerPin = -1,
+                uint8_t measurementsToAverage = 1);
+    /**
+     * @brief Destroy the SenseairK30 object
+     */
+    ~SenseairK30();
+
+    /**
+     * @copydoc Sensor::getSensorLocation()
+     */
+    String getSensorLocation(void) override;
+
+    /**
+     * @brief Do any one-time preparations needed before the sensor will be able
+     * to take readings.
+     *
+     * This sets pin mode on the trigger pin.  It also sets the expected stream
+     * timeout for modbus and updates the #_sensorStatus.  No sensor power is
+     * required.  This will always return true.
+     *
+     * @return **bool** True if the setup was successful.
+     */
+    bool setup(void) override;
+    /**
+     * @brief Wake the sensor up, if necessary.  Do whatever it takes to get a
+     * sensor in the proper state to begin a measurement.
+     *
+     * Verifies that the power is on and updates the #_sensorStatus.  This also
+     * sets the #_millisSensorActivated timestamp.
+     *
+     * For the K30, this also reads and dumps any returned "header" lines
+     * from the sensor.
+     *
+     * @note This does NOT include any wait for sensor readiness.
+     *
+     * @return **bool** True if the wake function completed successfully.
+     */
+    bool wake(void) override;
+
+    /**
+     * @copydoc Sensor::addSingleMeasurementResult()
+     */
+    bool addSingleMeasurementResult(void) override;
+
+ private:
+    int8_t  _triggerPin;
+    Stream* _stream;
+};
+
+
+/* clang-format off */
+/**
+ * @brief The Variable sub-class used for the
+ * [range output](@ref sensor_senseair_co2) from a
+ * [Senseair K30 Carbon dioxide senser](@ref sensor_senseairK30).
+ *
+ * @ingroup sensor_senseair
+ */
+/* clang-format on */
+class SenseairK30_CO2 : public Variable {
+ public:
+    /**
+     * @brief Construct a new SenseairK30_CO2 object.
+     *
+     * @param parentSense The parent SenseairK30 providing the result
+     * values.
+     * @param uuid A universally unique identifier (UUID or GUID) for the
+     * variable; optional with the default value of an empty string.
+     * @param varCode A short code to help identify the variable in files;
+     * optional with a default value of "CO2".
+     */
+    explicit SenseairK30_Range(SenseairK30* parentSense, const char* uuid = "",
+                               const char* varCode = K30_DEFAULT_CODE)
+        : Variable(parentSense, (const uint8_t)K30_VAR_NUM,
+                   (uint8_t)K30_RESOLUTION, K30_VAR_NAME, K30_UNIT_NAME,
+                   varCode, uuid) {}
+    /**
+     * @brief Construct a new SenseairK30_CO2 object.
+     *
+     * @note This must be tied with a parent SenseairK30 before it can be
+     * used.
+     */
+    SenseairK30_CO2()
+        : Variable((const uint8_t)K30_VAR_NUM, (uint8_t)K30_RESOLUTION,
+                   K30_VAR_NAME, K30_UNIT_NAME, K30_DEFAULT_CODE) {}
+    /**
+     * @brief Destroy the SenseairK30_CO2 object - no action needed.
+     */
+    ~SenseairK30_CO2() {}
+};
+/**@}*/
+#endif  // SRC_SENSORS_SENSEAIRK30_H_
